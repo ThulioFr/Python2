65ff9ccf73f1dab1c8d76750c0a6d3a9321f2f1f
THULIO FRANCO DE OLIVEIRA <thulio.foliveira@senacsp.edu.br>
2025-05-31 11:24:58 -0300

    3105

--- motor/core/views.py
+++ motor/core/views.py
@@ -4,11 +4,17 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
+def sobre(request):
+    return render(request, 'sobre.html')
+def servico(request):
+    return render(request, 'servicos.html')
+def veiculo(request):
+    return render(request, 'veiculo.html')
+
 def contato(request):
     context = {
-    'nome': 'MotorWeb',
-    'fone': '17189237198731',
-    'email': 'motorweb@teste.com.br'
-}
-
+        'nome':'MotorWeb',
+        'fone':'17-7070-6070',
+        'email':'motorweb@teste.com'
+    }
     return render(request, 'contato.html', context)
